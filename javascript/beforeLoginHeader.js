1fc26a2e3819d2c5cc5661ac74b73b9f35f44088
junyeon <jun981008@naver.com>
2023-11-30 21:14:53 +0900

    로그인창 로고 앵커 url 수정

--- javascript/beforeLoginHeader.js
+++ javascript/beforeLoginHeader.js
@@ -1,10 +1,15 @@
 function createHeader(){
   var header = document.querySelector("#header")
   var headerLogo = document.createElement("a");
-  var baseUrl = "http://15.164.221.192:8080/stageus_outsourcing/index.jsp"
+  var baseUrl1 = "http://15.164.221.192:8080/stageus_outsourcing/index.jsp"
+  var baseUrl2 = "http://15.164.221.192:8080/stageus_outsourcing/"
   headerLogo.textContent = "Stageus"
 
-  headerLogo.href = "../index.jsp"
+  if(window.location.href == baseUrl1 || window.location.href == baseUrl2){
+    headerLogo.href = "./index.jsp"
+  }else{
+    headerLogo.href = "../index.jsp"
+  }
   headerLogo.className = "headerLogo"
   header.appendChild(headerLogo)
 }
